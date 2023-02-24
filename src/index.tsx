4ba86aa5997014446529439a92b2e53ecb29ca86
andreybyrsa <byrsa2004@bk.ru>
2023-02-24 19:52:37 +0500

    Добавил fireBase

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+
+import store from './store';
+
+import './firebase';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
