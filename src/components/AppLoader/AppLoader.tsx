9488350fa5ca3b876e564198b5466b49baddabe4
andreybyrsa <byrsa2004@bk.ru>
2023-02-24 16:56:08 +0500

    Добавил компонент загрузки приложения

---
+++ src/components/AppLoader/AppLoader.tsx
@@ -0,0 +1,17 @@
+import { RingLoader } from 'react-spinners';
+import PageLayout from '../../layout';
+
+import './AppLoader.scss'
+
+function AppLoader() {
+  return (
+    <PageLayout contentClassName="app-loader">
+      <RingLoader
+        color="#6FBAF8"
+        size={100}
+      />
+    </PageLayout>
+  );
+}
+
+export default AppLoader;
